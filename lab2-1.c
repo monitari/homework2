f9a1b30edfc0105895f0a66643c3f3221b0e539e
monitari <qualified7389@gmail.com>
2024-03-16 16:08:52 +0900

    it was first commit

---
+++ lab2-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    char charType;                                            // char 타입 변수 charType 선언
+    int integerType;                                          // int 타입 변수 integerType 선언
+    float floatType;                                          // float 타입 변수 floatType 선언
+    double doubleType;                                        // double 타입 변수 doubleType 선언
+    printf("Size of char : %ldbyte\n", sizeof(charType));     // charType의 크기 : 1바이트
+    printf("Size of int : %ldbyte\n", sizeof(integerType));   // integerType의 크기 : 4바이트
+    printf("Size of float : %ldbyte\n", sizeof(floatType));   // floatType의 크기 : 4바이트
+    printf("Size of double : %ldbyte\n", sizeof(doubleType)); // doubleType의 크기 : 8바이트
+
+    printf("========== 김준후 2023041018 ==========\n");
+    printf("Size of char: %ldbyte\n", sizeof(char));      // char의 크기 : 1바이트
+    printf("Size of int: %ldbytes\n", sizeof(int));       // int의 크기 : 4바이트
+    printf("Size of float: %ldbytes\n", sizeof(float));   // float의 크기 : 4바이트
+    printf("Size of double: %ldbytes\n", sizeof(double)); // double의 크기 : 8바이트
+
+    printf("========== 김준후 2023041018 ==========\n");
+    printf("Size of char*: %ldbyte\n", sizeof(char *));      // 32비트 환경이므로
+    printf("Size of int*: %ldbytes\n", sizeof(int *));       // 포인터 변수의 크기는
+    printf("Size of float*: %ldbytes\n", sizeof(float *));   // 모두
+    printf("Size of double*: %ldbytes\n", sizeof(double *)); // 4바이트로 출력
+    return 0;
+}
+// 테스트
---
+++ lab2-2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main() {
+    int i;
+    int *ptr;
+    int **dptr;
+    i = 1234;
+
+    printf("[checking values before ptr = &i]\n"); // ptr = &i 할당 전
+    printf("value of i == %d\n", i);               // i = 1234
+    printf("address of i == %p\n", &i);            // i의 주소 = 0061FF1C
+    printf("value of ptr == %p\n", ptr);           // ptr의 값 = 00000000 (아직 할당 X)
+    printf("address of ptr == %p\n", &ptr);        // ptr의 주소 = 0061FF18
+
+    ptr = &i; // ptr = &i, ptr에 i의 주소(=0061FF1C) 할당
+    printf("========== 김준후 2023041018 ==========\n");
+    printf("[checking values after ptr = &i]\n");
+    printf("value of i == %d\n", i);        // i = 1234
+    printf("address of i == %p\n", &i);     // i의 주소 = 0061FF1C
+    printf("value of ptr == %p\n", ptr);    // ptr의 값 = i의 주소 = 0061FF1C
+    printf("address of ptr == %p\n", &ptr); // ptr의 주소 = 0061FF18
+    printf("value of *ptr == %d\n", *ptr);  //*ptr의 값 = i = 1234
+    dptr = &ptr;                            // dptr = &ptr, dptr에 ptr의 주소(=0061FF18) 할당
+
+    printf("========== 김준후 2023041018 ==========\n");
+    printf("[checking values after dptr = &ptr]\n");
+    printf("value of i == %d\n", i);           // i = 1234
+    printf("address of i == %p\n", &i);        // i의 주소 = 0061FF1C
+    printf("value of ptr == %p\n", ptr);       // ptr의 값 = i의 주소 = 0061FF1C
+    printf("address of ptr == %p\n", &ptr);    // ptr의 주소 = 0061FF18
+    printf("value of *ptr == %d\n", *ptr);     //*ptr의 값 = i = 1234
+    printf("value of dptr == %p\n", dptr);     // dptr의 값 = ptr의 주소 = 0061FF18
+    printf("address of dptr == %p\n", &dptr);  // dptr의 주소 = 0061FF14
+    printf("value of *dptr == %p\n", *dptr);   //*dptr의 값 = ptr의 값 = i의 주소 = 0061FF1C
+    printf("value of **dptr == %d\n", **dptr); //**dptr의 값 = *ptr의 값 = i = 1234
+
+    *ptr = 7777; //*ptr = 7777
+    printf("========== 김준후 2023041018 ==========\n");
+    printf("[after *ptr = 7777]\n");
+    printf("value of i == %d\n", i);           // i = *ptr의 값 = 7777, 즉 i = 7777
+    printf("value of *ptr == %d\n", *ptr);     //*ptr = 7777
+    printf("value of **dptr == %d\n", **dptr); //**dptr의 값 = *ptr = 7777, 즉 **dptr = 7777
+
+    **dptr = 8888; //*dptr = 8888
+    printf("========== 김준후 2023041018 ==========\n");
+    printf("[after **dptr = 8888]\n");
+    printf("value of i == %d\n", i);           // i = *ptr의 값 = **dptr = 8888, 즉 i = 8888
+    printf("value of *ptr == %d\n", *ptr);     //*ptr = **dptr = 8888, 즉 *ptr = 8888
+    printf("value of **dptr == %d\n", **dptr); //*dptr = 8888
+}
+//테스트
